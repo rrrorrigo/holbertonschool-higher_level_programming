30adb2524674bea8db49b3b7e970bd8f627bd34a
Rodrigo <rodrigobriones54@gmail.com>
2021-04-30 00:10:37 +0000

    solving checks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
         from sys import argv
         l = len(argv)
         if l != 4:
-                print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+                print("Usage: ./100-my_calculator.py <a> <operator> <b>")
                 exit(1)
         from calculator_1 import add, sub, mul, div
         operator = ["+","-","*","/"]
