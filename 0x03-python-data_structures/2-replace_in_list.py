ea50d77135df64ab9499805642fc3af1ac722b8e
Rodrigo <rodrigobriones54@gmail.com>
2021-05-04 21:27:19 +0000

    function that replaces an element of a list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+        if my_list and idx >= 0 and len(my_list) > idx:
+                my_list[idx] = element
+                return my_list
+        else:
+                return my_list
