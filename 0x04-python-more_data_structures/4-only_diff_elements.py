56781be225924ee0e1b6daacb57463b5cd23fa36
Rodrigo <rodrigobriones54@gmail.com>
2021-05-06 21:11:54 +0000

    changed method

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-        set_r = []
-        for string in set_1:
-                if string not in set_2:
-                        set_r.append(string)
-        for string2 in set_2:
-                if string2 not in set_1:
-                        set_r.append(string2)
-        return set_r
+        return set_1 ^ set_2
