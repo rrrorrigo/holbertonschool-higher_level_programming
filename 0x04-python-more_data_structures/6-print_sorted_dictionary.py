ce02893b84212de42e29b15f148ade25129cda3d
Rodrigo <rodrigobriones54@gmail.com>
2021-05-06 21:41:05 +0000

    tiping error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for s, s2 in sorted(a_dictionary.items()):
-        print("{} : {}".format(s, s2))
+        print("{}: {}".format(s, s2))
