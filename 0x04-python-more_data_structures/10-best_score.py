528c9a1b68627b883d0e2ea61f35228a0e1f2275
Rodrigo <rodrigobriones54@gmail.com>
2021-05-06 23:38:04 +0000

    solving errors

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return (max(a_dictionary) if a_dictionary else None)
+    #key= is a way to sort some result, in this case it sort if the value exist
+    return (max(a_dictionary, key=a_dictionary.get) if a_dictionary else None)
