f37ec3956faff9450498e00fd670b696a5ab6f6f
Rodrigo <rodrigobriones54@gmail.com>
2021-05-06 23:39:26 +0000

    solving pep8

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    #key= is a way to sort some result, in this case it sort if the value exist
+    # key= is a way to sort some result in this case it sort if the value exist
     return (max(a_dictionary, key=a_dictionary.get) if a_dictionary else None)
