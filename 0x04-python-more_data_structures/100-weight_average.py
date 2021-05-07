046b0d2b1e1feaef72d98c8c3cc2bc0c9eabaa4c
Rodrigo <rodrigobriones54@gmail.com>
2021-05-07 20:19:54 +0000

    fixed division by 0

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,10 @@
 def weight_average(my_list=[]):
     r = 0
     d = 0
+    result = 0
     if my_list:
         for i, ii in my_list:
             r += i * ii
             d += ii
-    result = r / d
+            result = r / d
     return result
