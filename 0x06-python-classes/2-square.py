693d08257c56a4925e331d900fee4d27cd5db362
Rodrigo <rodrigobriones54@gmail.com>
2021-05-18 20:23:57 +0000

    class that defines a square based on 1-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This is class named Square that defines a square """
+
+
+class Square:
+        """ Definition """
+        def __init__(self, size=0):
+                """ Initialization of private instance attribute size
+                which is asigned with the double underscore before given name
+
+                Args:
+                    size (int): private instance attribute.
+
+                Raises:
+                    TypeError: if `size` is not an integer
+                    ValueError: if `size` is less than zero
+
+                """
+                if type(size) is not int:
+                        raise TypeError("size must be an integer")
+                if size < 0:
+                        raise ValueError("size must be >=0")
+                self.__size = size
