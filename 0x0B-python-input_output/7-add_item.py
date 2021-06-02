38ec75a441bd0c48f1663599d8ab3961c221f5de
Rodrigo <rodrigobriones54@gmail.com>
2021-06-02 20:55:25 +0000

    added permissions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,9 +5,9 @@ import os.path
 
 if __name__ == '__main__':
 
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-    load_json_file = __import__('8-load_from_json_file').load_from_json_file
+    load_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     update = []
     f = "add_item.json"
