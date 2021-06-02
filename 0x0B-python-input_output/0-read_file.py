bff70582173c175fcacda9322f4ad3fe21d48567
Rodrigo <rodrigobriones54@gmail.com>
2021-06-02 20:51:10 +0000

    fixing error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ Read a file using `with` statement"""
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end='')
