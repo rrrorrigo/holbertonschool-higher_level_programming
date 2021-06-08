2dfd8db32797cfb95975a774ad7e0e6b334d4d1f
Rodrigo <rodrigobriones54@gmail.com>
2021-06-08 17:31:24 +0000

    fixing error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,6 +15,7 @@ class Base:
                         Base.__nb_objects += 1
                         self.id = self.__nb_objects
 
+        @staticmethod
         def to_json_string(list_dictionaries):
                 """ json to string method"""
                 l = list_dictionaries
