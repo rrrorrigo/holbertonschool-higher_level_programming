ac9762289a6401de4bb2a165f1eff7a270d78dc8
Rodrigo <rodrigobriones54@gmail.com>
2021-06-08 17:20:16 +0000

    fixin error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,8 @@ class Base:
                 """ json to string method"""
                 if list_dictionaries is None or len(list_dictionaries) != 0:
                         return json.dumps(list_dictionaries)
-                return "[]"
+                else:
+                        return "[]"
 
         @classmethod
         def save_to_file(cls, list_objs):
