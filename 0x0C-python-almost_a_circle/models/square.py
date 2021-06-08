6666f1db2861c0e1a8c42e817aaa6aa341c0cbb2
Rodrigo <rodrigobriones54@gmail.com>
2021-06-08 16:35:18 +0000

    trying to fixing an unknown eror

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ from rectangle import Rectangle
 class Square(Rectangle):
         """ class square with init method"""
         def __init__(self, size, x=0, y=0, id=None):
+                """ initializer method"""
                 super().__init__(size, size, x, y, id)
 
         def __str__(self):
