75b1ce12535f739f14157fd5922586bc9633c598
rrrorrigo <rodrigobriones54@gmail.com>
2021-06-16 13:30:23 -0300

    advanced task that swap != with ==

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,10 +4,10 @@
 
 class MyInt(int):
         """ class Myint that swap == with !="""
-        def __eq__(self, sign):
+        def __eq__(self, other):
                 """ swap == with !="""
-                return int.__ne__(self, sign)
+                return int.__ne__(self, other)
 
-        def __ne__(self, sign):
+        def __ne__(self, other):
                 """ swap != with =="""
-                return int.__eq__(self, sign)
+                return int.__eq__(self, other)
