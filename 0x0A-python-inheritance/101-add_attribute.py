dcb204090ff913e0260efbce856ebb7e30bb5c6a
rrrorrigo <rodrigobriones54@gmail.com>
2021-06-16 13:45:16 -0300

    function that add new attribute if it exist

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,7 +2,7 @@
 """create class MyInt"""
 
 
-def addAttribute(obj, attribute, nattribute):
+def add_attribute(obj, attribute, nattribute):
         """ function that add new attribute if it exists"""
         if not hasattr(obj, "__dict__"):
                 raise TypeError("can't add new attribute")
