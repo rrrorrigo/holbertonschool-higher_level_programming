1db1ac0ec7dd252022f1f7b537c73507e7e2fa7b
Rodrigo <rodrigobriones54@gmail.com>
2021-07-13 23:53:05 +0000

    upload file

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,9 +3,9 @@
 CREATE SCHEMA IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 CREATE IF NOT EXISTS `cities` (
-       id INT UNIQUE AUTO_INCREMENT NOT NULL
-       state_id INT NOT NULL,
+       `id` INT UNIQUE AUTO_INCREMENT NOT NULL
+       `state_id` INT NOT NULL,
        name VARCHAR(256) NOT NULL,
-       PRIMARY KEY (id),
-       FOREIGN KEY (state_id) REFERENCES states(id)
+       PRIMARY KEY (`id`),
+       FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)
 );
