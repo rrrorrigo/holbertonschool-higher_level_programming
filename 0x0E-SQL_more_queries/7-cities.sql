2b5b2457dbbbe7c04c9afe8a7d285ba915ffc2d5
Rodrigo <rodrigobriones54@gmail.com>
2021-07-13 23:57:43 +0000

    upload file

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- VALUES: id PRIMARY KEY, state_id FOREIGN KEY from id of states, name
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS cities (
        id INT UNIQUE AUTO_INCREMENT NOT NULL
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
