4057c078585a18ffa76025f2ab20423cd071c8c9
Rodrigo <rodrigobriones54@gmail.com>
2021-07-14 03:17:42 +0000

    idk what i have wrong

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,5 @@
--- script that lists all shows from hbtn_0d_tvshows_rate by their rating
--- Each record should display: tv_shows.title - rating sum
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows INNER JOIN tv_show_ratings
-ON tv_shows.id = tv_show_ratings.show_id
+-- script that lists all shows by their rating
+-- Each record should display: tv_shows.title  rating sum
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id ORDER BY rating DESC;
