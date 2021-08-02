9d91a7b430e66e7c6c15e710a43e655b27ac4b2d
Rodrigo <rodrigobriones54@gmail.com>
2021-08-02 21:13:14 +0000

    upload file

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const av = process.argv;
+let r = 2;
+let max = 2;
+if (av.length > 3) {
+  for (let index = 2; index < av.length; index++) {
+    if (parseInt(av[max]) < parseInt(av[index])) {
+      max = index;
+    }
+  }
+  for (let ii = 2; ii < av.length; ii++) {
+    if (parseInt(av[r]) < parseInt(av[ii]) && ii !== max) {
+      r = ii;
+    }
+  }
+  console.log(av[r]);
+} else {
+  console.log('0');
+}
