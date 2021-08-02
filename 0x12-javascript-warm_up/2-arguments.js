e4c7824ea803162856d444cb349c0a2b78622bd2
rrrorrigo <rodrigobriones54@gmail.com>
2021-08-02 15:37:15 -0300

    upload files

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const l = process.argv.length;
+if (l === 2) {
+  console.log('No argument');
+} else if (l === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
