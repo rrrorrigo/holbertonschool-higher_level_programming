51dd7b09bcb9c1f4ed33e3816e0816f238be8838
Rodrigo <rodrigobriones54@gmail.com>
2021-08-04 00:11:38 +0000

    trying to fix an unknown error

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,12 +2,12 @@
 const av = process.argv;
 const fd = require('fs');
 if (av.length > 4) {
-  fd.readFile(av[2], 'utf-8', function (err, data) {
+  fd.readFile('./' + av[2], 'utf-8', function (err, data) {
     if (!err) {
-      fd.readFile(av[3], 'utf-8', function (err, data2) {
+      fd.readFile('./' + av[3], 'utf-8', function (err, data2) {
         if (!err) {
-          const s = data + '\n' + data2;
-          fd.writeFile(av[4], s, function () {});
+          const s = data + data2;
+          fd.writeFile('./' + av[4], s, function () {});
         }
       });
     }
