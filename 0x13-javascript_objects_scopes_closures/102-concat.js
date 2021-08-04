84b96a1e0e46b72fe8a48ffa9355661556392b81
Rodrigo <rodrigobriones54@gmail.com>
2021-08-04 00:03:09 +0000

    upload advanced task

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const av = process.argv;
+const fd = require('fs');
+fd.readFile(av[2], 'utf-8', function (err, data) {
+  if (!err) {
+    fd.readFile(av[3], 'utf-8', function (err, data2) {
+      if (!err) {
+        const s = data + '\n' + data2;
+        fd.writeFile(av[4], s, function () {});
+      }
+    });
+  }
+});
