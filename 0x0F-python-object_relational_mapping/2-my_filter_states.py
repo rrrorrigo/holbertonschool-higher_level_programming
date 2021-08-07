061278f2be0b0cd3565b2f6f30ff8627056f59af
Rodrigo <rodrigobriones54@gmail.com>
2021-08-07 20:27:33 +0000

    added case-sensitive comparision

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         conn = sql.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
         cur = conn.cursor()
-        cur.execute("SELECT id, name FROM states WHERE name = '{}'"
+        cur.execute("SELECT id, name FROM states WHERE name LIKE BINARY '{}'"
                     .format(argv[4]))
         query_rows = cur.fetchall()
         for row in query_rows:
