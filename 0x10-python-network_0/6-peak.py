72184fc50ba3b0205a55233c109405c033ffa166
Rodrigo <rodrigobriones54@gmail.com>
2021-08-19 23:23:06 +0000

    changed method

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,14 @@
 def find_peak(list_of_integers):
         """function that finds a peak"""
         l = list_of_integers
-        if l and l.count(l[0]) == len(l):
+        if not l:
+                return None
+        if len(l) == 1:
                 return l[0]
-        for i in range(1, len(list_of_integers) - 1):
-                if l[i] > l[i - 1] and l[i] > l[i + 1]:
-                        return l[i]
+        middle = int(len(l) / 2)
+        if l[middle] > l[middle + 1] and l[middle] > l[middle - 1]:
+                return l[middle]
+        if l[middle] < l[middle + 1] and l[middle] > l[middle - 1]:
+                return l[middle + 1]
+        else:
+                return max(l)
