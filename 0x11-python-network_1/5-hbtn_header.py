13dc259fac2b08c995f760b2d7e9d6e26032a29a
Rodrigo <rodrigobriones54@gmail.com>
2021-08-20 22:32:37 +0000

    added argument func

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,10 +2,11 @@
 """ Python script that takes in a URL, sends a request to the URL
 and displays the value of the variable X-Request-Id in the response header"""
 import requests
+from sys import argv
 
 
 if __name__ == '__main__':
-        url = "https://intranet.hbtn.io"
+        url = argv[1]
         value = "X-Request-Id"
         r = requests.get(url)
         print(r.headers[value])
