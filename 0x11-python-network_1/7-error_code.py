bc416d5f2ce7d62237e0ed0936082bd096e3afa5
Rodrigo <rodrigobriones54@gmail.com>
2021-08-20 22:55:10 +0000

    upload file

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request
+to the URL and displays the body of the response"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+        url = argv[1]
+        r = requests.get(url)
+        try:
+                print(r.text)
+        except:
+                print("Error code: {}".format(r.status_code))
