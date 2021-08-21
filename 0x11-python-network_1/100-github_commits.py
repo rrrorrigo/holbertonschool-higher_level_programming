ff11567152bd796444d5d48ed350ca1244554b2d
Rodrigo <rodrigobriones54@gmail.com>
2021-08-21 03:00:51 +0000

    upload file

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+        repository = argv[1]
+        owner = argv[2]
+        r = requests.get("https://api.github.com/repos/{}/{}/commits"
+                         .format(repository, owner))
+        data = r.json()
+        for i, req in enumerate(data):
+                if i < 10:
+                        sha = req.get('sha')
+                        author = req.get('commit').get('author').get('name')
+                        print("{}: {}".format(sha, author))
+                else:
+                        break
