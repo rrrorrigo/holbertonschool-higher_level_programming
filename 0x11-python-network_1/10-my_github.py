be4e05b5de65362fc40e8123919af3c18bbf1ac5
Rodrigo <rodrigobriones54@gmail.com>
2021-08-21 00:13:33 +0000

    upload file

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+        usr = argv[1]
+        pwd = argv[2]
+        r = requests.get('https://api.github.com/user', auth=(usr, pwd))
+        data = r.json()
+        print(data['id'])
