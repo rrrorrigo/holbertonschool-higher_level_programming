52f130e6ab2e6b29287f1403fb259f1e296dde34
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-22 23:32:17 -0300

    fixed semistandard

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
   $('INPUT#language_code').keypress(function (event) {
-      if(event.which === 13){
-        const lang = $('INPUT#language_code').val();
-        $.get("https://www.fourtonfish.com/hellosalut/?lang=" + lang, function (data, status) {
-          $('DIV#hello').text(data.hello);
-        }); 
-      }
+    if (event.which === 13) {
+      const lang = $('INPUT#language_code').val();
+      $.get('https://www.fourtonfish.com/hellosalut/?lang=' + lang, function (data, status) {
+        $('DIV#hello').text(data.hello);
+      });
+    }
   });
   $('INPUT#btn_translate').click(function () {
     const lang = $('INPUT#language_code').val();
-    $.get("https://www.fourtonfish.com/hellosalut/?lang=" + lang, function (data, status) {
+    $.get('https://www.fourtonfish.com/hellosalut/?lang=' + lang, function (data, status) {
       $('DIV#hello').text(data.hello);
     });
   });
