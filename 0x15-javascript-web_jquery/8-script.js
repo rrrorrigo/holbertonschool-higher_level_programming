cc8f3459d845fc05bb6a10de2c81d4c82612b017
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-22 23:29:54 -0300

    fixed semistandard

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data, status) {
     data.results.forEach(obj => {
-      $('UL#list_movies').append("<li>" + obj.title + "</li>");
+      $('UL#list_movies').append('<li>' + obj.title + '</li>');
     });
   });
 });
