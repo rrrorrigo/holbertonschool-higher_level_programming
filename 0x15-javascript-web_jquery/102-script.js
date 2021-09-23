59534c4120a507a6bd2b5f2a382dcc8caffe60d7
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-22 23:06:59 -0300

    upload file

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('INPUT#btn_translate').click(function () {
+    const lang = $('INPUT#language_code').val();
+    $.get("https://www.fourtonfish.com/hellosalut/?lang=" + lang, function (data, status) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
