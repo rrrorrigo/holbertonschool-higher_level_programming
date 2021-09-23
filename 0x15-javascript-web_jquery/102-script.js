d586218a5e54f255400666265e59132e58a53f19
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-22 23:30:43 -0300

    fixed semistandard

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('INPUT#btn_translate').click(function () {
     const lang = $('INPUT#language_code').val();
-    $.get("https://www.fourtonfish.com/hellosalut/?lang=" + lang, function (data, status) {
+    $.get('https://www.fourtonfish.com/hellosalut/?lang=' + lang, function (data, status) {
       $('DIV#hello').text(data.hello);
     });
   });
